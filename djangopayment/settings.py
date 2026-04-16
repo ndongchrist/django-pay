5d73472a934ecaa54ed1990120d7da127939f4c7
ndongchrist <christianhonore2003@gmail.com>
2026-04-16 15:46:22 +0100

    feat: Configured postgres sb

--- djangopayment/settings.py
+++ djangopayment/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = "djangopayment.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# let configure a postgresql database using environment variables
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "djangopayment"),
+        "USER": os.getenv("POSTGRES_USER", "goldenbrain"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "11n08n03"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
